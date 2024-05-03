4660ade43623649e8bc27f22d64b5d0443054901
Яриханов Хасан <hyarihanov@corp.local>
2024-05-03 17:36:45 +0300

    Changes

--- src/main/java/org/yarikhanov_khasan/multithreading/Foo.java
+++ src/main/java/org/yarikhanov_khasan/multithreading/Foo.java
@@ -1,15 +1,45 @@
 package org.yarikhanov_khasan.multithreading;
 
+import java.util.concurrent.Semaphore;
+
 public class Foo {
+    private final Semaphore s;
+
+    public Foo(Semaphore s) {
+        this.s = s;
+    }
+
+
     public void first() {
-        System.out.print("first");
+        try {
+            s.acquire();
+            System.out.print("first");
+        }catch (InterruptedException e) {
+            System.err.println(e.getMessage());
+        }finally {
+            s.release();
+        }
     }
 
     public void second() {
-        System.out.print("second");
+        try {
+            s.acquire();
+            System.out.print("second");
+        }catch (InterruptedException e) {
+            System.err.println(e.getMessage());
+        }finally {
+            s.release();
+        }
     }
 
     public void third() {
-        System.out.print("third");
+        try {
+            s.acquire();
+            System.out.print("third");
+        }catch (InterruptedException e) {
+            System.err.println(e.getMessage());
+        }finally {
+            s.release();
+        }
     }
 }
--- src/main/java/org/yarikhanov_khasan/multithreading/Main.java
+++ src/main/java/org/yarikhanov_khasan/multithreading/Main.java
@@ -5,8 +5,9 @@ import java.util.concurrent.Semaphore;
 public class Main {
     public static void main(String[] args) {
         Semaphore semaphore = new Semaphore(1);
-        new FirstThread(semaphore);
-        new SecondThread(semaphore);
-        new ThirdThread(semaphore);
+        Foo foo = new Foo(semaphore);
+        new Thread(foo::first).start();
+        new Thread(foo::second).start();
+        new Thread(foo::third).start();
     }
 }
